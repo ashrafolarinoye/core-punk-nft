dc06a0ea8c9733f0593a9edc7f0f56299740117c
ashrafolarinoye <ashrafolarinoye@gmail.com>
2023-02-18 00:24:28 +0100

    init

---
+++ assets/functions.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+	jQuery('#open-mobile-menu').click(function(){
+		jQuery('.custom-menu-links').addClass('mobile-menu-opened');
+	});
+
+	jQuery('#close-mobile-menu').click(function(){
+		jQuery('.custom-menu-links').removeClass('mobile-menu-opened');
+	})
+})
